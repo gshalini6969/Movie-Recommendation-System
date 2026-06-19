5f52637670213ab0886802364ead70f2374cdbd3
Shalini <g.shalini6969@gmail.com>
2026-06-19 16:15:11 +0530

    Added genre search and preprocessing

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-print("Movie Recommendation system")
-movies = {"Avatar" : ["Titanic","The Abyss"],"Titanic" : ["Avatar","The Notebook"],"Inception" : ["Interstellar","Tenet"]}
-inp=input("Enter movie name: ")
-if inp in movies:
-    print("Recommended movies: ")
-    for movie in movies[inp]:
-        print(movie)
-else:
-    print("Movie not found")
-print("Hello you!")
+import pandas as pd
+
+movies = pd.read_csv("movies.csv")
+
+movie_name = input("Enter Movie: ").lower()
+
+# Step 1: Find genre
+genre=movies[movies["Movie"].str.lower()==movie_name]["Genre"][0]
+print(genre)
+# Step 2: Find movies with same genre
+same_genre=movies[movies["Genre"]==genre]
+print(same_genre)
+# Step 3: Remove entered movie
+recommendations=same_genre[same_genre["Movie"].str.lower() != movie_name]
+# Step 4: Print recommendations
+print(recommendations)
